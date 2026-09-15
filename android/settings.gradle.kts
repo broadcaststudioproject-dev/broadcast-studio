789722164b2def1aa1879224aaaa77043625f2d1
vinodkumarj <broadcaststudioproject@gmail.com>
2026-09-15 21:04:32 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -17,7 +17,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.ALLOW_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
