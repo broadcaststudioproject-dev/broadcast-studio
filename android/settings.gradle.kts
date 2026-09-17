699f63e56b8dc93d4df58e28f180bc3a031c9b88
vinodkumarj <broadcaststudioproject@gmail.com>
2026-09-17 21:20:18 +0530

    Downgrade Android application plugin version to 8.3.2

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,7 +6,6 @@ pluginManagement {
         require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
     }
-
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
@@ -27,7 +26,7 @@ dependencyResolutionManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.9.1" apply false
+    id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
