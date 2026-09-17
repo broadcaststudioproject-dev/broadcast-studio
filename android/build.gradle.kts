21a376f7e6ba6a7a927bc767824f641b79d35923
vinodkumarj <broadcaststudioproject@gmail.com>
2026-09-17 21:46:46 +0530

    Add clean task and update build directory path
    Add clean task to delete build directory and update build directory path.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,10 @@ allprojects {
     }
 }
 
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
