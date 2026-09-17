02defc30b97b03a579dde5d50173d5e8665f6fa5
vinodkumarj <broadcaststudioproject@gmail.com>
2026-09-17 12:51:08 +0530

    Change Java version compatibility to 1.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,9 +10,12 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+kotlinOptions {
+    jvmTarget = "1.8"
+}
 
     packaging {
         resources.excludes.add("project.clj")
